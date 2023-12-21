f207013cea97091e0024ae9d2c5da54f8f90f69b
martincervino <78787810+martincervino@users.noreply.github.com>
2023-12-21 23:00:13 +0100

    apartado 2

--- Trabajo_espacial.R
+++ Trabajo_espacial.R
@@ -48,32 +48,44 @@ load("./datos_trabajo/temp_andalu_8.RData")
 #' 
 #' # Ejercicios
 #' 
-#' 1.  Lectura de datos y análisis exploratorio
+#' ---
 #' 
-#'     a. Realizar un breve análisis descriptivo del conjunto de datos y generar un 
-#'        histograma de la respuesta.
+#' #### **1.  Lectura de datos y análisis exploratorio**
+#' 
+#' 
+#' **_a) Realizar un breve análisis descriptivo del conjunto de datos y generar un 
+#' histograma de la respuesta._**
 
 head(temp_andalu)
 str(temp_andalu)
 summary(temp_andalu)
+attr(temp_andalu, "fecha") # Mes de las observaciones
+#' Las temperaturas se tomaron en enero de 2022. <br>
 
-#' Asignamos a una variable a la temperatura de la que vamos a realizar un análisis descriptivo unidimensional 
+#' Asignamos a una variable a la temperatura de la que vamos a realizar un análisis descriptivo unidimensional.
 t <- temp_andalu$temp 
 summary(t)
 
 #' Histograma de la respuesta:
 hist(t, xlab = "temperatura", main = "", freq = FALSE) 
 lines(density(t), col = 'blue')
-#' La distribución no es una normal, hay una ligera asimetría
+#' La distribución no es una normal, hay una ligera asimetría.
 
-#'
-#'     b. Convertir el `data.frame` a un objeto `sf`, definiendo el CRS mediante la 
-#'        cadena de texto `"+proj=utm +zone=30 +ellps=WGS84 +units=km"`, y representar 
-#'        la distribución espacial de la respuesta. ¿Se observa algún patrón 
-#'        (más o menos claro)?
+qqnorm(t)
+qqline(t)
+
+#' Test de normalidad de Shapiro-Wilk:
+shapiro.test(t)
+#' Según el test no hay normalidad. 
+#' 
+#' ***
+#' 
+#' **_b) Convertir el `data.frame` a un objeto `sf`, definiendo el CRS mediante la 
+#' cadena de texto `"+proj=utm +zone=30 +ellps=WGS84 +units=km"`, y representar 
+#' la distribución espacial de la respuesta. ¿Se observa algún patrón (más o menos claro)?_**
 #'
 
-#' Vamos a convertir los datos a un objeto sf,
+#' Vamos a convertir los datos a un objeto sf.
 #' Mantenemos lan y lot como posibles variables expicativas.
 crs <- "+proj=utm +zone=30 +ellps=WGS84 +units=km" # Definimos el CRS
 
@@ -81,15 +93,16 @@ temp_andalu_sf <- st_as_sf(temp_andalu, coords = c("lon", "lat"), remove = FALSE
 
 
 
-#' Representación de la distribución espacial de la respuesta.
+#' Representación de la distribución espacial de la respuesta, asignando un color dependiendo de la temperatura medida en cada observación.
 plot(temp_andalu_sf["temp"], pch = 20, cex = 2, breaks = "quantile", nbreaks = 4)
 
-#' Se observa en general temperatura mayor en en los puntos del suroeste y una menor en los puntos del noreste.
-
-#'       
-#'     c. Completar el análisis descriptivo de la variabilidad de gran escala. 
-#'        ¿Aparentemente hay tendencia espacial? En caso afirmativo, 
-#'        sugerir un modelo lineal. 
+#' Se observa en general temperatura mayor en en los puntos del sur y suroeste y una menor en los puntos del norte y noreste.
+#' Estos patrones nos hacen pensar que la tendencia no es constante.
+#' 
+#' ***
+#'     
+#' **_c) Completar el análisis descriptivo de la variabilidad de gran escala. 
+#' ¿Aparentemente hay tendencia espacial? En caso afirmativo, sugerir un modelo lineal._**
 #'        
 
 #' Completamos el analisis descriptivo de la variabilidad de gran escala con unos 
@@ -105,9 +118,9 @@ lines(lowess(y, t), lty = 2, lwd = 2, col = 'blue')
 
 par(mfrow = c(1,1))
 
-#' Se observa como las componentes espaciales tienen un efecto en la respuesta por lo que es coherente
-#' pensar que existe dependencia espacial.
-#' Sugerimos un modelo lineal temp ~ lon + lat
+#' Se observa como las dos componentes espaciales, tanto como la longitud como la latitud
+#' tienen un efecto en la respuesta por lo que es lógico pensar que existe dependencia espacial.
+#' Una tendencia lineal parece adecuada, sugerimos un modelo lineal temp ~ lon + lat
 
 #' Ajustamos el modelo por ols
 temp.ols <- lm(temp ~ lon + lat, data = temp_andalu_sf)
@@ -121,12 +134,19 @@ summary(res)
 hist(res, xlab = "ols residuals", main = "", freq = FALSE)
 lines(density(res), col = 'blue')
 #' Los residuos se asemejan bastante a una distribución normal
-#' 
 
+qqnorm(res)
+qqline(res)
+
+#' Test de normalidad de Shapiro-Wilk
+shapiro.test(res)
+#' Hay normalidad en los residuos
+#' 
+#' ***
 #'    
-#'     d. Volver a representar la distribución espacial de la respuesta (o solo las posiciones 
-#'        de observación si surgen dificultades) junto con los límites administrativos de
-#'        Andalucía (que se pueden obtener empleando el paquete `mapSpain`).
+#' **_d) Volver a representar la distribución espacial de la respuesta (o solo las posiciones 
+#' de observación si surgen dificultades) junto con los límites administrativos de
+#' Andalucía (que se pueden obtener empleando el paquete `mapSpain`)._**
 #'        
 
 
@@ -138,37 +158,123 @@ andalucia_limites <- mapSpain::esp_get_ccaa(ccaa="andalucia")
 #' Creamos los cuantiles para la distribuir los colores
 quantiles <- quantile(temp_andalu_sf$temp, probs = c(0, 0.25, 0.5, 0.75, 1))
 
-#' Hacemos el grafico usando ggplot juntando los limites de andalucía junto a 
+#' Hacemos el grafico usando ggplot juntando los límites de Andalucía junto a 
 #' la representación de la distribución espacial de la respuesta
 #' 
 ggplot() +
   geom_sf(data = andalucia_limites, fill = "transparent", color = "black", lwd = 1) +
   geom_sf(data = temp_andalu_sf, aes(color = temp_andalu_sf$temp), pch = 20, cex = 5) +
-  scale_color_gradient(breaks = quantiles, low = "blue", high = "red", name = "Temperature")
+  scale_color_gradient(breaks = quantiles, low = "blue", high = "red", name = "Temperatura") +
+  ggtitle("Temperatura en el mes de enero de 2022 en la comunidad de Andalucía")
 
 
-#' 2. Modelado de la dependencia espacial
+
+#' #### **2. Modelado de la dependencia espacial**
 #' 
-#'     a. Analiza la variabilidad de pequeña escala empleando el estimador clásico 
-#'        del semivariograma, considerando 20 saltos hasta un salto máximo de 290.
+#' **_a) Analiza la variabilidad de pequeña escala empleando el estimador clásico 
+#' del semivariograma, considerando 20 saltos hasta un salto máximo de 290._**
 #'        
 
+library(gstat)
 
+maxlag <- 290 # Salto máximo de 290
+# Estimador clásico del semivariograma-> cressie = False
+vario <- variogram(temp ~ lon + lat, temp_andalu_sf, cutoff = maxlag, width = maxlag/20)
 
+#' Represantación de las estimaciones junto con el número de aportaciones:
+plot(vario, plot.numbers = TRUE)
 
-#'     b. Ajustar a las estimaciones piloto un modelo (isotrópico) de semivariograma
-#'        exponencial y uno esférico, mediante WLS. ¿Con cuál se obtiene un mejor ajuste?
-#'        Interpretar las estimaciones obtenidas de los parámetros del semivariograma.
-#'        
-#'     c. Emplear medidas de validación cruzada, considerando 10 grupos, para seleccionar 
-#'        el modelo final (establecer la semilla igual al número de grupo multiplicado 
-#'        por 10; no es necesario generar gráficos). 
-#'        
-#' 3. Predicción espacial
+#' Observamos que las tres primeras estimaciones tienen una semivarianza baja
+#' comparada con el resto. A partir de la tercera estimación hay un salto de semivarianza.
 #' 
-#'     a. Crear una rejilla de predicción de dimensiones 150x75 que cubra los límites 
-#'        de Anadalucía e intersecarla con dicha región (o una rejilla que cubra las 
-#'        posiciones de observación si surgen dificultades).
+#' ***
+#' 
+#' **_b. Ajustar a las estimaciones piloto un modelo (isotrópico) de semivariograma
+#' exponencial y uno esférico, mediante WLS. ¿Con cuál se obtiene un mejor ajuste?
+#' Interpretar las estimaciones obtenidas de los parámetros del semivariograma._**
+#'
+#'
+#' Primero definimos el modelo exponencial con _vgm()_:
+modelo_exp <- vgm(model = "Exp", nugget = NA) # Valores iniciales por defecto
+#' Ajustamos el modelo con _fit.variogram()_ empleando pesos inversamente proporcionales a la varianza (_fit.method = 2_)
+fit_exp <- fit.variogram(vario, modelo_exp, fit.method = 2)
+fit_exp
+#' Asignamos a variables para trabajar con ellas posteriormente
+nugget_exp <- fit_exp$psill[1]
+sill_exp <- nugget_exp + fit_exp$psill[2]
+#' En el caso de un variograma exponencial, el parámetro que aparece como range 
+#' es un parámetro de escala proporcional al verdadero rango práctico (tres veces ese valor).
+#' Asi que multiplicamos por 3:
+range_exp <- 3*fit_exp$range[2]
+#' Parámetros del semivariograma:
+params_exp <- matrix(c("nugget_exp", "sill_exp", "range_exp", nugget_exp, sill_exp, range_exp), 2, 3, byrow = TRUE)
+params_exp
+#' INTERPRETAR PARÁMETROS 
+
+
+plot(vario, fit_exp)
+
+plot(vario$dist, vario$gamma, xlab = "distance", ylab = "semivariance",
+     xlim = c(0, max(range_exp*1.1, maxlag)), ylim = c(0, sill_exp*1.2))
+lines(variogramLine(fit_exp, maxdist = max(range_exp*1.1, maxlag)))
+abline(v = 0, lty = 3)
+abline(v = range_exp, lty = 3)
+abline(h = nugget_exp, lty = 3)
+abline(h = sill_exp, lty = 3)
+
+#' Ahora definimos el modelo esférico:
+modelo_sph <- vgm(model = "Sph", nugget = NA) # Valores iniciales por defecto
+#' Ajustamos el modelo con _fit.variogram()_ empleando pesos inversamente proporcionales a la varianza (_fit.method = 2_)
+fit_sph <- fit.variogram(vario, modelo_sph, fit.method = 2)
+fit_sph
+#' Asignamos a variables para trabajar con ellas posteriormente
+nugget_sph <- fit_sph$psill[1]
+sill_sph <- nugget_sph + fit_sph$psill[2]
+#' En el caso de un variograma esférico el parametro _range_ ya es el verdadero rango práctico.
+range_sph <- fit_sph$range[2]
+#' Parámetros del semivariograma:
+params_sph <- matrix(c("nugget_sph", "sill_sph", "range_sph", nugget_sph, sill_sph, range_sph), 2, 3, byrow = TRUE)
+params_sph 
+
+#' INTERPRETAR PARÁMETROS 
+
+
+plot(vario, fit_sph)
+
+plot(vario$dist, vario$gamma, xlab = "distance", ylab = "semivariance",
+     xlim = c(0, max(range_sph*1.1, maxlag)), ylim = c(0, sill_sph*1.2))
+lines(variogramLine(fit_sph, maxdist = max(range_sph*1.1, maxlag)))
+abline(v = 0, lty = 3)
+abline(v = range_sph, lty = 3)
+abline(h = nugget_sph, lty = 3)
+abline(h = sill_sph, lty = 3)
+
+
+#' Vamos a comparar los dos modelo a través del valor mínimo de la función objetivo WLS. <br>
+#' Modelo exponencial:
+attr(fit_exp, "SSErr")
+#' Modelo esférico:
+attr(fit_sph, "SSErr")
+#' Parece que el modelo esférico obtiene un mejor ajuste, acontinuación haremos un validación
+#' cruzada, (mas recomendable). 
+
+
+#' 
+#' ***
 #'        
-#'     b. Empleando el modelo obtenido en el ejercicio anterior, calcular las 
-#'        prediccies y varianzas kriging en la rejilla de predicción, y representarlas.
+#' **_c) Emplear medidas de validación cruzada, considerando 10 grupos, para seleccionar 
+#'    el modelo final (establecer la semilla igual al número de grupo multiplicado 
+#'    por 10; no es necesario generar gráficos). _**
+#'        
+#' #### **3. Predicción espacial**
+#' 
+#' **_a. Crear una rejilla de predicción de dimensiones 150x75 que cubra los límites 
+#'    de Anadalucía e intersecarla con dicha región (o una rejilla que cubra las 
+#'    posiciones de observación si surgen dificultades)._**
+#'        
+
+
+
+#'        
+#'  **_b. Empleando el modelo obtenido en el ejercicio anterior, calcular las 
+#'    prediccies y varianzas kriging en la rejilla de predicción, y representarlas._**
